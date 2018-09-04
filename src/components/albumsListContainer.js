90fc5ffabc7cc511f32a3dafa16425938c5dcf41
MIVD <mi.vandeurzen@gmail.com>
2018-09-05 00:21:13 +0200

    complete upto thunks-section

--- src/components/albumsListContainer.js
+++ src/components/albumsListContainer.js
@@ -1,34 +1,17 @@
-
 import * as React from 'react'
-import AlbumsList from './albumsList'
+import * as request from 'superagent'
 import { connect } from 'react-redux'
+import AlbumsList from './albumsList'
 import { setAlbums } from '../actions/albums'
-// import AddAlbumForm from './AddAlbumForm'
-
-const sleep = time => new Promise(
-  resolve => setTimeout(() => resolve(`I waited for ${time} ms`), time)
-)
 
 class AlbumsListContainer extends React.PureComponent {
   componentDidMount() {
-    sleep(2000)
-      .then(message => this.props.setAlbums([
-        {
-          id: 1,
-          title: message
-        },
-        {
-          id: 2,
-          title: 'This is the second album'
-        },
-        {
-          id: 3,
-          title: 'The last album'
-        }
-      ]))
+    request('https://jsonplaceholder.typicode.com/albums')
+      .then(response => this.props.setAlbums(response.body))
   }
 
   render() {
+    console.log(this.props.albums)
     if (!this.props.albums) return 'Loading...'
     return (<div>
       <AlbumsList albums={this.props.albums} />
